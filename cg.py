42aab9d2683715d6e7ffaa18e3d182f73603a4fb
Chetanghotekar <chetan.ghotekar_24uds@sanjivani.edu.in>
2025-04-23 19:50:23 +0530

    Update cg.py

--- cg.py
+++ cg.py
@@ -5,3 +5,7 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Chetanghotekar/community1.git
 git push -u origin main
+
+git remote add origin https://github.com/Chetanghotekar/community1.git
+git branch -M main
+git push -u origin main
